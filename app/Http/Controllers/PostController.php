9f6359860503c941704f39324cd2277331c2f182
MyUsername <emanuele.marini37@gmail.com>
2021-09-14 00:06:45 +0200

    migration -nothing to???

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Post;
 
 class PostController extends Controller
 {
       public function homepage(){
-
+            
+            $allPosts = Post::all();
             return view('home');
         }
 }

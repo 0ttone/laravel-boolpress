dc7cb9411047e3a3f3b3e6d38d2cb291d32ca84b
MyUsername <emanuele.marini37@gmail.com>
2021-09-15 00:25:21 +0200

    index ok

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,7 +1,27 @@
 @extends('layouts.app')
 
 @section('content')
-
-QUI VANNO I POST
+<div class="container post-container">
+      <table class="table">
+      <thead>
+      <tr>
+            <th scope="col">#</th>
+            <th scope="col">title</th>
+            <th scope="col">content</th>
+            <th scope="col">img</th>
+      </tr>
+      </thead>
+      <tbody>
+            @foreach($allPosts as $post)
+            <tr>
+                  <th scope="row">{{$post->id}}</th>
+                  <td>{{$post->title}}</td>
+                  <td>{{$post->content}}</td>
+                  <td><img src="{{$post->img}}" alt="image of{{$post->title}}"/></td>
+            </tr>
+            @endforeach
+      </tbody>
+      </table>
+</div>
 
 @endsection
